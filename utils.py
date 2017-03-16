e4f4e4c7f20ef2dab17e8c0293b52b5cbcd05a3d
poignant <poignant@decore0.imag.fr>
2017-03-16 14:52:02 +0100

    add comments

--- utils.py
+++ utils.py
@@ -10,6 +10,9 @@ import random, os
 import numpy as np
 
 def ComputeMean(imagesList, depth=256):
+    """
+    Compute the mean value of each RGB channel of a set of images
+    """
     r,g,b,i = 0.0, 0.0, 0.0, 0.0
     for img in imagesList:
         try:
@@ -23,6 +26,9 @@ def ComputeMean(imagesList, depth=256):
     return r/i/depth, g/i/depth, b/i/depth
 
 def ComputeStdDev(imagesList, depth=256):
+    """
+    Compute the standard deviation value of each RGB channel of a set of images
+    """
     r,g,b,i = 0.0, 0.0, 0.0, 0.0
     for img in imagesList:
         try:
@@ -36,6 +42,9 @@ def ComputeStdDev(imagesList, depth=256):
     return r/i/depth, g/i/depth, b/i/depth
 
 def copyFeaturesParametersAlexnet(net, netBase):
+    """
+    Copy the feature part parameters of a pretrained AlexNet model
+    """
     for i, f in enumerate(net.features):
         if type(f) is torch.nn.modules.conv.Conv2d:
             print "copy", f
@@ -43,6 +52,9 @@ def copyFeaturesParametersAlexnet(net, netBase):
             f.bias.data = netBase.features[i].bias.data
 
 def copysClassifierParameterAlexnet(net, netBase):
+    """
+    Copy the classifier part parameters of a pretrained AlexNet model
+    """
     for i, c in enumerate(net.classifier):
         if type(c) is torch.nn.modules.linear.Linear :
             if c.weight.size() == netBase.classifier[i].weight.size():
@@ -52,6 +64,9 @@ def copysClassifierParameterAlexnet(net, netBase):
 
 
 def copyFeaturesParametersResnet50(net, netBase):
+    """
+    Copy all parameters of a Resnet 50 layer from a pretrained model (except for the last fully connected layer)
+    """    
     net.conv1.weight.data = netBase.conv1.weight.data
     net.bn1.weight.data = netBase.bn1.weight.data
     net.bn1.bias.data = netBase.bn1.bias.data
@@ -78,19 +93,38 @@ def copyFeaturesParametersResnet50(net, netBase):
         targetLayer[0].downsample[1].bias.data = rootLayer[0].downsample[1].bias.data  
 
 
-def trainclassifier(net, optimizer, criterion, batchSize, dImgTrain, imSize, imageTransform, dClassImgTrain, IdOutput):
+def trainclassifier(net, optimizer, criterion, batchSize, dImg, imSize, imageTransform, dClassImg, IdOutput):
+    """
+    Train a net:
+        net: the neural network (for example: net = models.alexnet())
+        optimizer: the optimizer (for example: optimizer = optim.SGD(alexTunedClassifier.parameters(), lr=0.001, momentum=0.9)) 
+        batchSize: size of the batch, dependinf on the GPU memory available (batchSize = 32)
+        dImg: dictionary with as key the image name and for values the image open with PIL
+        imSize: image size (imSize=225 for alexnet, resnet, ...)
+        imageTransform: composition of transformation that will be done on images, 
+            For example:
+                imageTrainTransform = transforms.Compose([transforms.Scale(300), 
+                                                          transforms.RandomCrop(225), 
+                                                          transforms.ToTensor(), 
+                                                          transforms.Normalize(mean = mean,
+                                                                               std = std),
+                                                         ])
+            see transforms.Compose in pytorch for more information
+        dClassImg: dictionary with as key the image name and for value the corresponding class
+        IdOutput: position of the output of the net(inputs) result to used in the criterion
+    """
     net.train()
     # shuffle images name 
-    lImgName = dImgTrain.keys()
+    lImgName = dImg.keys()
     random.shuffle(lImgName)
     # Split the whole list into sublist sizeof batch_size
     for subListImgName in [lImgName[i:i+batchSize] for i in range(0, len(lImgName), batchSize)][:-1]:
         # transform images into tensor
         inputs = torch.Tensor(batchSize, 3, imSize, imSize).cuda()
-        for i, imgName in enumerate(subListImgName): inputs[i] = imageTransform(dImgTrain[imgName])
+        for i, imgName in enumerate(subListImgName): inputs[i] = imageTransform(dImg[imgName])
         inputs = Variable(inputs)  
         # list class of the sublist images
-        lab = Variable(torch.LongTensor([dClassImgTrain[imgName] for imgName in subListImgName]).cuda())
+        lab = Variable(torch.LongTensor([dClassImg[imgName] for imgName in subListImgName]).cuda())
         # zero the parameter gradients
         optimizer.zero_grad()
         # forward + backward + optimize
@@ -100,17 +134,32 @@ def trainclassifier(net, optimizer, criterion, batchSize, dImgTrain, imSize, ima
         optimizer.step()
 
 
-def testClassifier(net, dImgTest, imSize, imageTransform, dClassImgTrain, IdOutput):
+def testClassifier(net, dImg, imSize, imageTransform, dClassImg, IdOutput):
+    """
+    Test a neural network on a test dataset
+        net: the neural network (for example: net = models.alexnet())
+        dImg: dictionary with as key the image name and for values the image open with PIL
+        imSize: image size (imSize=225 for alexnet, resnet, ...)
+        imageTransform: composition of transformation that will be done on images, 
+            For example:
+                imageTestTransform = transforms.Compose([transforms.ToTensor(), 
+                                                         transforms.Normalize(mean = mean,
+                                                                              std = std),
+                                                        ]))
+            see transforms.Compose in pytorch for more information
+        dClassImg: dictionary with as key the image name and for value the corresponding class
+        IdOutput: position of the output of the net(inputs) result to used in the criterion
+    """
     net.eval()
     nbCorrect = 0
-    for imgName in dImgTest:
+    for imgName in dImg:
         inp = torch.Tensor(1,3, imSize, imSize).cuda()
-        inp[0] = imageTransform(dImgTest[imgName])
+        inp[0] = imageTransform(dImg[imgName])
 
         outputs = net(Variable(inp))[IdOutput]
         _, predicted = torch.max(outputs.data, 1)
         predicted = predicted.tolist()        
-        nbCorrect+= (predicted[0][0] == dClassImgTrain[imgName])
+        nbCorrect+= (predicted[0][0] == dClassImg[imgName])
 
-    print "test : #Correct "+str(nbCorrect)+" on "+str(len(dImgTest))+" ("+str(round(float(nbCorrect)/float(len(dImgTest))*100, 1))+"%)"
+    print "test : #Correct "+str(nbCorrect)+" on "+str(len(dImg))+" ("+str(round(float(nbCorrect)/float(len(dImg))*100, 1))+"%)"
 
