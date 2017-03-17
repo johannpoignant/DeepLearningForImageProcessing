b5df8cb77d7a723340cfc0492a47b73a071a7da3
poignant <poignant@decore0.imag.fr>
2017-03-17 10:52:03 +0100

    generalize copy parameter resnet

--- utils.py
+++ utils.py
@@ -63,34 +63,67 @@ def copysClassifierParameterAlexnet(net, netBase):
                 c.bias.data = netBase.classifier[i].bias.data 
 
 
-def copyFeaturesParametersResnet50(net, netBase):
+def copyFeaturesParametersResnet(net, netBase, nbBlock1, nbBlock2, nbBlock3, nbBlock4, typeBlock="Bottleneck"):
     """
-    Copy all parameters of a Resnet 50 layer from a pretrained model (except for the last fully connected layer)
-    """    
+    Copy all parameters of a Resnet model from a pretrained model (except for the last fully connected layer)
+    typeBlock == "BasicBlock" for resnet18 and resnet34 or "Bottleneck" for resnet50, resnet101 and resnet152
+    resnet18: nbBlock1, nbBlock2, nbBlock3, nbBlock4 = 2, 2, 2, 2
+    resnet34: nbBlock1, nbBlock2, nbBlock3, nbBlock4 = 3, 4, 6, 3
+    resnet50: nbBlock1, nbBlock2, nbBlock3, nbBlock4 = 3, 4, 6, 3
+    resnet101: nbBlock1, nbBlock2, nbBlock3, nbBlock4 = 3, 4, 23, 3
+    resnet152: nbBlock1, nbBlock2, nbBlock3, nbBlock4 = 3, 8, 36, 3
+    see model/resnet.py for more informations about the model
+    """
+
+    if typeBlock not in ["BasicBlock", "Bottleneck"]:
+        print 'error in the block name, choose "BasicBlock", "Bottleneck"'
+        return
+
+    print "copy net.conv1", net.conv1
     net.conv1.weight.data = netBase.conv1.weight.data
+    print "copy net.bn1", net.bn1
     net.bn1.weight.data = netBase.bn1.weight.data
     net.bn1.bias.data = netBase.bn1.bias.data
 
-    lLayer = [(net.layer1, netBase.layer1, 3),
-              (net.layer2, netBase.layer2, 4),
-              (net.layer3, netBase.layer3, 6),
-              (net.layer4, netBase.layer4, 3)
+    lLayer = [("layer1", net.layer1, netBase.layer1, nbBlock1),
+              ("layer2", net.layer2, netBase.layer2, nbBlock2),
+              ("layer3", net.layer3, netBase.layer3, nbBlock3),
+              ("layer4", net.layer4, netBase.layer4, nbBlock4)
              ]
 
-    for targetLayer, rootLayer, nbC in lLayer:
-        for i in range(nbC):
-            targetLayer[i].conv1.weight.data = rootLayer[i].conv1.weight.data
-            targetLayer[i].bn1.weight.data = rootLayer[i].bn1.weight.data
-            targetLayer[i].bn1.bias.data = rootLayer[i].bn1.bias.data
-            targetLayer[i].conv2.weight.data = rootLayer[i].conv2.weight.data
-            targetLayer[i].bn2.weight.data = rootLayer[i].bn2.weight.data
-            targetLayer[i].bn2.bias.data = rootLayer[i].bn2.bias.data
-            targetLayer[i].conv3.weight.data = rootLayer[i].conv3.weight.data
-            targetLayer[i].bn3.weight.data = rootLayer[i].bn3.weight.data
-            targetLayer[i].bn3.bias.data = rootLayer[i].bn3.bias.data
-        targetLayer[0].downsample[0].weight.data = rootLayer[0].downsample[0].weight.data
-        targetLayer[0].downsample[1].weight.data = rootLayer[0].downsample[1].weight.data
-        targetLayer[0].downsample[1].bias.data = rootLayer[0].downsample[1].bias.data  
+    if typeBlock == "BasicBlock":
+        for layerName, targetLayer, rootLayer, nbC in lLayer:
+            print "copy", layerName, rootLayer
+            for i in range(nbC):
+                targetLayer[i].conv1.weight.data = rootLayer[i].conv1.weight.data
+                targetLayer[i].bn1.weight.data = rootLayer[i].bn1.weight.data
+                targetLayer[i].bn1.bias.data = rootLayer[i].bn1.bias.data
+                targetLayer[i].conv2.weight.data = rootLayer[i].conv2.weight.data
+                targetLayer[i].bn2.weight.data = rootLayer[i].bn2.weight.data
+                targetLayer[i].bn2.bias.data = rootLayer[i].bn2.bias.data
+            if targetLayer[0].downsample:
+                targetLayer[0].downsample[0].weight.data = rootLayer[0].downsample[0].weight.data
+                targetLayer[0].downsample[1].weight.data = rootLayer[0].downsample[1].weight.data
+                targetLayer[0].downsample[1].bias.data = rootLayer[0].downsample[1].bias.data 
+
+    elif typeBlock == "Bottleneck":
+        for layerName, targetLayer, rootLayer, nbC in lLayer:
+            print "copy", layerName, rootLayer
+            for i in range(nbC):
+                targetLayer[i].conv1.weight.data = rootLayer[i].conv1.weight.data
+                targetLayer[i].bn1.weight.data = rootLayer[i].bn1.weight.data
+                targetLayer[i].bn1.bias.data = rootLayer[i].bn1.bias.data
+                targetLayer[i].conv2.weight.data = rootLayer[i].conv2.weight.data
+                targetLayer[i].bn2.weight.data = rootLayer[i].bn2.weight.data
+                targetLayer[i].bn2.bias.data = rootLayer[i].bn2.bias.data
+                targetLayer[i].conv3.weight.data = rootLayer[i].conv3.weight.data
+                targetLayer[i].bn3.weight.data = rootLayer[i].bn3.weight.data
+                targetLayer[i].bn3.bias.data = rootLayer[i].bn3.bias.data
+            targetLayer[0].downsample[0].weight.data = rootLayer[0].downsample[0].weight.data
+            targetLayer[0].downsample[1].weight.data = rootLayer[0].downsample[1].weight.data
+            targetLayer[0].downsample[1].bias.data = rootLayer[0].downsample[1].bias.data 
+
+
 
 
 def trainclassifier(net, optimizer, criterion, batchSize, dImg, imSize, imageTransform, dClassImg, IdOutput):
